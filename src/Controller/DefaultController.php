c773b8c0f90914562b0fe3291c971e9a25c1b1f3
Memooh <m.ovdb@outlook.com>
2019-11-19 16:40:42 +0100

    Admin Index

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use JMS\SecurityExtraBundle\Annotation\Secure;
 
 class DefaultController extends AbstractController
 {
@@ -16,4 +17,17 @@ class DefaultController extends AbstractController
             'controller_name' => 'DefaultController',
         ]);
     }
+
+    /**
+     * @Route("/adminindex", name="adminindex")
+     */
+    public function adminIndex()
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'Unable to access this page!');
+
+        return $this->render('admin/adminindex.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
 }
